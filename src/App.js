74a31029030c506cc137310fc48102d4816cd598
Kirill Egorov <kirill.egorov@x5.ru>
2023-03-27 16:19:06 +0200

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import Router from './pages/Router'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Router />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
